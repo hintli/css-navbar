81ad36e6216599a823118eb35986d23c0c1fee0a
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-10-24 17:11:57 +0300

    express,routing,path

---
+++ app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve('./index.html'));
+})
+
+app.get('/about', function (req, res) {
+    res.sendFile(path.resolve('./about.html'));
+})
+
+app.get('/contact', function (req, res) {
+    res.sendFile(path.resolve('./contact.html'));
+})
+
+app.get('/users/:userID/movies/:movieID', (req,res) => {
+    res.send(`<h1>${req.params.userID} </h1>
+        <h1>${req.params.movieID} </h1>`)
+})
+ 
+app.listen(3001)

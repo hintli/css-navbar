787928f47993b424b5bf3f8590764a2253622400
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-10-24 13:37:21 +0300

    request,response

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
-const port = 3002;
+const port = 3005;
 const hostname = '127.0.0.1';
 
-const server = http.createServer((req,res) => {
-    console.log(req.url);
-    res.setHeader('Content-Type','text/plain')
-    res.statusCode = 200;
-    res.end("Server calisiyor");
+const IndexPage = fs.readFileSync('index.html');
+const ContactPage = fs.readFileSync('contact.html');
+const AboutPage = fs.readFileSync('about.html');
 
-})
+
+const server = http.createServer((req,res) => {
+    if(req.url === '/') {
+        return res.end(IndexPage)
+    }
+    else if(req.url === '/contact') {
+        return res.end(ContactPage);
+    }
+    else if(req.url === '/about') {
+        return res.end(AboutPage);
+    }
+    else {
+        res.statusCode = 404
+        res.end('Error')
+    }
+    
+});
 
 
 server.listen(port,hostname,() => {
-    console.log(`Server Calisiyor, http://${hostname}:${port}/`)
+    console.log(`Server Listening || htttp://${hostname}:${port}`);
 });

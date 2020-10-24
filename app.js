43c4c14608721bd2467e4fe5f83bdd4877d1ab96
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-10-24 17:59:15 +0300

    proje start

--- app.js
+++ app.js
@@ -1,30 +1,19 @@
-const path = require('path');
 const express = require('express')
-const app = express()
-
+const app = express ()
+const path = require('path')
 
 app.use(express.static('public'))
 
-// app.use('/test',(req,res,next) => {
-//     console.log('Middleware')
-//     next();
-// })
- 
-app.get('/', function (req, res) {
-    res.sendFile(path.resolve('./index.html'));
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'site/index.html'))
 })
 
-app.get('/about', function (req, res) {
-    res.sendFile(path.resolve('./about.html'));
+app.get('/about', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'site/about.html'))
 })
 
-app.get('/contact', function (req, res) {
-    res.sendFile(path.resolve('./contact.html'));
+app.get('/blog', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'site/blog.html'))
 })
 
-app.get('/users/:userID/movies/:movieID', (req,res) => {
-    res.send(`<h1>${req.params.userID} </h1>
-        <h1>${req.params.movieID} </h1>`)
-})
- 
 app.listen(3001)
